d8de82049cf0f492aa221301593432e39ccdf514
frostbane <frostbane@programmer.net>
2021-08-31 00:57:57 +1000

    add repeat until macro

---
+++ macros/repeat_until.hpp
@@ -0,0 +1,9 @@
+#ifndef FB_HTTP_SERVER_MACRO_REPEAT_UNTIL_HPP__
+#define FB_HTTP_SERVER_MACRO_REPEAT_UNTIL_HPP__
+
+/** @brief repeat loop until condition is met */
+#define repeat do
+/** @brief repeat loop until condition is met */
+#define until(condition) while(!(condition))
+
+#endif /* FB_HTTP_SERVER_MACRO_REPEAT_UNTIL_HPP__ */
